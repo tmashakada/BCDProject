da6f8f705191d3be700300026d9133addd216555
tafadzwa mashakada <tmashakada10@gmail.com>
2021-07-09 08:42:55 +0200

    initial code

---
+++ src/test/java/com/mashsoft/TestBinaryCodeDecimal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mashsoft;
+
+import com.mashsoft.bcdproject.BinaryCodedDecimal;
+import org.junit.Test;
+
+/**
+ *
+ * @author tmashakada email:tmashakada10@gmail.com
+ */
+public class TestBinaryCodeDecimal {
+    
+   @Test 
+   public void testBcd(){
+       
+       BinaryCodedDecimal binaryCodedDecimal=new BinaryCodedDecimal();
+       
+   }
+}
